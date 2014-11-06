b6ee9b45d858eecb815937c0ea2e680a8e25d9f7
Johan Air <johan@nexys.ch>
2014-11-06 02:29:38 -0500

    added doc sample

---
+++ documentation_sample.scala
@@ -0,0 +1,20 @@
+/*
+	this documents standards when documenting code
+
+
+	source: http://docs.scala-lang.org/style/scaladoc.html
+
+*/
+
+
+// methods and function
+
+
+/*
+	brief descritption of what it does, sometimes obvious [optional]
+	@param name: name of employee
+	@param age: employee's age
+	@return: probabiltiy of leaving company
+*/
+def score(name: String, age: Long): Double
+
